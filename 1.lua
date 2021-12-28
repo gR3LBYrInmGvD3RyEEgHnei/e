d86638162a86ef2134909a7e18e2524ef9114534
gR3LBYrInmGvD3RyEEgHnei <96791938+gR3LBYrInmGvD3RyEEgHnei@users.noreply.github.com>
2021-12-28 19:28:52 +0100

    Create 1.lua

---
+++ 1.lua
@@ -0,0 +1,325 @@
+-- ###########################
+-- #                         #
+-- #   - FLOOR 2 SCRIPT -    #
+-- #  SCRIPTED BY LYALEKIN   # 
+-- #     [+] UPD 20          #
+-- #                         #
+-- ###########################
+-- # FOR NOVA USERS
+
+-- # ADMIN PANEL: (Fly, qefly false and noclip)
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+
+-- # Portal
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(2863, 3, 1037)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+humanoid:ChangeState(Enum.HumanoidStateType.Flying)
+pl.CFrame = location
+wait(9)
+
+-- # 1
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(383, 370, -225)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+humanoid:ChangeState(Enum.HumanoidStateType.Flying)
+pl.CFrame = location
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(1) do
+       vim:SendKeyEvent(true, "B", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "U", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "G", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "H", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "Y", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "N", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "K", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "J", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "L", false, game)
+       wait(.9)
+peanut = peanut + 1
+if peanut == 1 then
+break
+end
+end       
+
+-- # 2
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(104, 383, -290)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+humanoid:ChangeState(Enum.HumanoidStateType.Flying)
+pl.CFrame = location
+wait(1)
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(-6, 384, -287)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+humanoid:ChangeState(Enum.HumanoidStateType.Flying)
+pl.CFrame = location
+wait(1)
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(77, 384, -281)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+humanoid:ChangeState(Enum.HumanoidStateType.Flying)
+pl.CFrame = location
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(3) do
+       vim:SendKeyEvent(true, "Q", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "Y", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "U", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "Z", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "K", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "L", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "M", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "R", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "N", false, game)
+       wait(.9)
+peanut = peanut + 1
+if peanut == 1 then
+break
+end
+end       
+
+-- # 3
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(-164, 383, -268)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+humanoid:ChangeState(Enum.HumanoidStateType.Flying)
+pl.CFrame = location
+wait(1)
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(-217, 383, -196)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+humanoid:ChangeState(Enum.HumanoidStateType.Flying)
+pl.CFrame = location
+wait(1)
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(-172, 383, -251)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+humanoid:ChangeState(Enum.HumanoidStateType.Flying)
+pl.CFrame = location
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(1) do
+       vim:SendKeyEvent(true, "E", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "B", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "Q", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "Y", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "U", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "K", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "L", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "M", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "R", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "N", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "K", false, game)
+       wait(.9)
+peanut = peanut + 1
+if peanut == 1 then
+break
+end
+end       
+
+-- # 4
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(-187, 437, 211)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+humanoid:ChangeState(Enum.HumanoidStateType.Flying)
+pl.CFrame = location
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(2) do
+       vim:SendKeyEvent(true, "Q", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "Y", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "U", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "K", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "L", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "M", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "R", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "N", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "Z", false, game)
+       wait(.9)
+peanut = peanut + 1
+if peanut == 1 then
+break
+end
+end       
+-- # 5
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(-264, 466, 523)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+humanoid:ChangeState(Enum.HumanoidStateType.Flying)
+pl.CFrame = location
+wait(1)
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(2) do
+       vim:SendKeyEvent(true, "Q", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "Y", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "Z", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "U", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "H", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "G", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "K", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "L", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "M", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "R", false, game)
+       wait(.9)
+       vim:SendKeyEvent(true, "N", false, game)
+       wait(.9)
+peanut = peanut + 1
+if peanut == 1 then
+break
+end
+end       
+
+
+-- # Boss
+local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(-255, 445, 960)
+local humanoid = game.Players.LocalPlayer.Character.Humanoid
+humanoid:ChangeState(Enum.HumanoidStateType.Flying)
+pl.CFrame = location
+wait(2)
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(.00) do
+       vim:SendKeyEvent(true, "Q", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "E", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "R", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "T", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "Y", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "F", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "U", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "P", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "F", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "L", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "K", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "J", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "H", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "G", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "F", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "M", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "N", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "B", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "F", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "F", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "F", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "F", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "F", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "F", false, game)
+       wait(.0)
+peanut = peanut + 1
+end       
+wait(.0)
+local vim = game:service'VirtualInputManager'
+local peanut = 0
+while wait(.00) do
+       vim:SendKeyEvent(true, "E", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "R", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "T", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "Y", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "U", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "P", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "L", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "K", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "J", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "H", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "G", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "F", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "H", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "G", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "F", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "M", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "N", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "B", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "F", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "F", false, game)
+       wait(.0)
+       vim:SendKeyEvent(true, "F", false, game)
+	   peanut = peanut + 1
+end       
